5f490a7a5553340890d073c096f0299dbc6f2fd2
bruno <brunocpessoal@gmail.com>
2019-10-30 11:24:27 +0000

    done styling the feed

---
+++ src/pages/Feed/Feed.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+
+import './Feed.css';
+
+import more from "../../assets/more.svg";
+import like from "../../assets/like.svg";
+import comment from "../../assets/comment.svg";
+import send from "../../assets/send.svg";
+
+class Feed extends Component {
+  render() {
+    return (
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>usuario</span>
+              <span className="place">Local</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img src="http://localhost:3333/files/instagram01.jpg" alt="" />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt=""/>
+              <img src={comment} alt=""/>
+              <img src={send} alt=""/>
+            </div>
+
+            <strong>900 curtidas</strong>
+
+            <p>
+              Um post muito massa!
+              <span>#react #javascript</span>
+            </p>
+          </footer>
+        </article>
+
+        <article>
+          <header>
+            <div className="user-info">
+              <span>usuario</span>
+              <span className="place">Local</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img src="http://localhost:3333/files/instagram01.jpg" alt="" />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt=""/>
+              <img src={comment} alt=""/>
+              <img src={send} alt=""/>
+            </div>
+
+            <strong>900 curtidas</strong>
+
+            <p>
+              Um post muito massa!
+              <span>#react #javascript</span>
+            </p>
+          </footer>
+        </article>
+
+        <article>
+          <header>
+            <div className="user-info">
+              <span>usuario</span>
+              <span className="place">Local</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img src="http://localhost:3333/files/instagram01.jpg" alt="" />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt=""/>
+              <img src={comment} alt=""/>
+              <img src={send} alt=""/>
+            </div>
+
+            <strong>900 curtidas</strong>
+
+            <p>
+              Um post muito massa!
+              <span>#react #javascript</span>
+            </p>
+          </footer>
+        </article>
+      </section>
+    );
+  };
+}
+
+export default Feed;
